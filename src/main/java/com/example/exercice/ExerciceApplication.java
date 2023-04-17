b1f7131a244063b68bb26b724349eef7a8a0c144
FaniryKeziah <hei.keziah.2@gmail.com>
2023-04-17 16:12:41 +0300

    fix: change packages

--- src/main/java/com/example/exercice/ExerciceApplication.java
+++ src/main/java/com/example/exercice/ExerciceApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan(basePackages = "com/example/exercice/exercice")
+@ComponentScan(basePackages = "com.example.exercice.exercice")
 public class ExerciceApplication {
 
 	public static void main(String[] args) {
